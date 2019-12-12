65646cee5dbcffa4fee4e6ca9beca41cddf4eca5
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-12-11 23:41:44 -0800

    Currently kinda works

--- nfa.py
+++ nfa.py
@@ -20,9 +20,11 @@ for lines in file:
     arr += [myList]
 file.close()
 
+listOfLambdas = []
 for i in range(len(arr)):
+    # print("array:", arr)
     if ((arr[i][0], arr[i][1]) in obj):
-        # print("first if array", (arr[i][0], arr[i][1]))
+        # print(i, "first if array", (arr[i][0], arr[i][1]))
         valList = []
         # print("array:", arr)
         for j in range(i-1, len(arr)):
@@ -37,23 +39,32 @@ for i in range(len(arr)):
                 valList.append(arr[j][2])
                 # valList.append(arr[j][2])
                 # print("valList:", valList)
-        if isinstance(obj[(arr[i][0], arr[i][1])], list):
+        # obj[(arr[i][0], arr[i][1])] = valList
+        # print("valList:", valList)
+        # print("obj", obj[(arr[i][0], arr[i][1])])
+        if (isinstance(obj[(arr[i][0], arr[i][1])], list) and (len(obj[(arr[i][0], arr[i][1])]) > 1)):
             break
         obj[(arr[i][0], arr[i][1])] = valList
+        # print("valList2:", valList)
+        # print("obj", obj[(arr[i][0], arr[i][1])])
     elif (len(arr[i]) == 3):
-        # print("second if array", (arr[i][0], arr[i][1]))
+        # print(i, "second if array", (arr[i][0], arr[i][1]))
         obj[(arr[i][0], arr[i][1])] = [arr[i][2]]
         # print("obj", obj)
     elif (len(arr[i]) == 2):
-        # print("third if array", (arr[i][0], arr[i][1]))
-        obj[(arr[i][0], 'lambda')] = [arr[i][1]]
+        if ((arr[i][0], 'lambda') in obj):
+            listOfLambdas += [arr[i][1]]
+            obj[(arr[i][0], 'lambda')] = listOfLambdas
+        else:
+            listOfLambdas += [arr[i][1]]
+            obj[(arr[i][0], 'lambda')] = listOfLambdas
         # print("obj", obj)
 
 # print("obj", obj)
 
 nfa = obj
 
-# print(nfa)
+print(nfa)
 
 # for i in range(len(arr)):
 #     if (arr[i][0] in obj and not (len(arr[i]) == 2)):
@@ -107,7 +118,7 @@ def accept(nfa, start_state, accept_states, string):
             k = k + len(nfa[states])
 
     max_length = k + (1 + k)*len(string)
-    return acceptNFA(nfa, start_state, accept_states, string, 0, 0, max_length)
+    return acceptNFA(nfa, start_state, accept_states, string, 0, max_length)
 
 # def accept(nfa, start, ends, string):
 #     k = 0
@@ -131,74 +142,43 @@ def accept(nfa, start_state, accept_states, string):
 # }
 
 
-def acceptNFA(nfa, start_state, accept_states, string, i, edges, max_length):
-    nextStates = start_state
-    # print("i", i)
+def acceptNFA(nfa, start_state, accept_states, string, i, max_length):
     if (i >= len(string)):
-        # print("hi")
         if (start_state in accept_states):
             return True
-        return False
+        else:
+            return False
+        if lambdaNFA(nfa, start_state, accept_states, string, i, max_length):
+            return True
+    nextStates = start_state
     for transitions in nfa:
-        # print(transitions)
-        # print(string[i])
-        if (string[i] in transitions and nextStates in transitions):
-            # print("transitions", transitions)
-            # print("nfa[transitions]", nfa[transitions])
+        if (string[i] in transitions and start_state in transitions):
             nextStates = nfa[transitions]
-            # print("nextStates", nextStates)
             break
-        # else:
-        #     return False
+        elif lambdaNFA(nfa, start_state, accept_states, string, i, max_length):
+            return True
+    print(nextStates)
     for next in nextStates:
-        if acceptNFA(nfa, next, accept_states, string, i+1, edges+1, max_length):
+        if acceptNFA(nfa, next, accept_states, string, i+1, max_length):
+            return True
+        if lambdaNFA(nfa, start_state, accept_states, string, i, max_length):
             return True
     return False
 
-    # if (edges > max_length and string != ''):
-    #     return False
-    # if (i >= len(string)):
-    #     if (start_state in accept_states):
-    #         return True
-    #     if lambdaNFA(nfa, start_state, accept_states, string, i, edges+1, max_length):
-    #         return True
-    #     return False
-    # if (string[i] in )
 
-    # if (string[i] in nfa[start_state]):
-    #     nextStates = nfa[start_state][string[i]]
-    # elif lambdaNFA(nfa, start_state, accept_states, string, i, edges, max_length):
-    #     return True
-    # else:
-    #     return False
-    # for curr in nextStates:
-    #     if acceptNFA(nfa, curr, accept_states, string, i+1, edges+1, max_length):
-    #         return True
-    # if lambdaNFA(nfa, start_state, accept_states, string, i, edges, max_length):
-    #     return True
+def lambdaNFA(nfa, start_state, accept_states, string, i, max_length):
+    for states in nfa:
+        if ('lambda' in states):
+            for current_state in nfa[states]:
+                if acceptNFA(nfa, current_state, accept_states, string, i, max_length):
+                    return True
+        return False
+
+    # if ('lamda' in nfa[start_state]):
+    #     for curr in nfa[start_state]['lamda']:
+    #         if acceptNFA(nfa, curr, accept_states, string, i, max_length):
+    #             return True
     # return False
-
-# def acceptNFA(nfa, start, ends, string, i, edges, max_length):
-#     if (edges > max_length and string != ''):
-#         return False
-#     if (i >= len(string)):
-#         if (start in ends):
-#             return True
-#         if lamdaNFA(nfa, start, ends, string, i, edges+1, max_length):
-#             return True
-#         return False
-#     if (string[i] in nfa[start]):
-#         nextStates = nfa[start][string[i]]
-#     elif lamdaNFA(nfa, start, ends, string, i, edges, max_length):
-#         return True
-#     else:
-#         return False
-#     for curr in nextStates:
-#         if acceptNFA(nfa, curr, ends, string, i+1, edges+1, max_length):
-#             return True
-#     if lamdaNFA(nfa, start, ends, string, i, edges, max_length):
-#         return True
-#     return False
 
 
 # def lambdaNFA(nfa, start, ends, string, i, edges, max_length):
@@ -213,3 +193,39 @@ if accept(nfa, start_state, accept_states, string):
     print('Accepted')
 else:
     print('Rejected')
+
+
+# def acceptNFA(nfa, start_state, accept_states, string, i, edges, max_length):
+#     nextStates = start_state
+#     print("i", i)
+#     if (edges > max_length and string != ''):
+#         return False
+#     if (i >= len(string)):
+#         if (nextStates in accept_states):
+#             print("whattup")
+#             return True
+#         else:
+#             print("bro")
+#             return False
+#         # if lambdaNFA(nfa, start_state, accept_states, string, i, edges+1, max_length):
+#         #     return True
+#     for transitions in nfa:
+#         print("transitions", transitions)
+#         print("string[i]", string[i])
+#         if (string[i] in transitions and nextStates in transitions):
+#             # print("transitions", transitions)
+#             # print("nfa[transitions]", nfa[transitions])
+#             nextStates = nfa[transitions]
+#             print("nextStates", nextStates)
+#             break
+#         # elif lambdaNFA(nfa, start_state, accept_states, string, i, edges, max_length):
+#         #     return True
+#         # else:
+#         #     return False
+#     for next in nextStates:
+#         print("next", next)
+#         if acceptNFA(nfa, next, accept_states, string, i+1, edges+1, max_length):
+#             return True
+#     # if lambdaNFA(nfa, start_state, accept_states, string, i, edges, max_length):
+#     #     return True
+#     return False
